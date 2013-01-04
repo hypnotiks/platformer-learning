9fceea22860fc63d3722aa1b9947f797d3729cef
unknown <24g@EEIGMPT.(none)>
2013-01-04 01:35:08 -0500

    First commit of code. Finished Tutorial 5 from CodingMadeEasy.

---
+++ Platformer/Platformer/Platformer/FadeAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer
+{
+    public class FadeAnimation : Animation
+    {
+        bool increase;
+        float fadeSpeed;
+        TimeSpan defaultTime, timer;
+        bool startTimer;
+        float activateValue;
+
+        //ENDED AT TUTORIAL 5
+
+        public override void LoadContent(ContentManager Content, Texture2D image, string text, Vector2 position)
+        {
+            base.LoadContent(Content, image, text, position);
+            increase = false;
+            fadeSpeed = 1.0f;
+            defaultTime = new TimeSpan(0, 0, 1);
+            timer = defaultTime;
+            activateValue = 0.0f;
+        }
+    }
+}
---
+++ Platformer/Platformer/Platformer/Screens/TitleScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Platformer
+{
+    public class TitleScreen : GameScreen
+    {
+        KeyboardState keyState;
+        SpriteFont font;
+
+        public override void LoadContent(ContentManager Content)
+        {
+            base.LoadContent(Content);
+            if (font == null)
+                font = content.Load<SpriteFont>("Font1");
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            keyState = Keyboard.GetState();
+            if (keyState.IsKeyDown(Keys.Enter))
+                ScreenManager.Instance.AddScreen(new SplashScreen());
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(font, "TitleScreen",
+                new Vector2(100, 100), Color.Black);
+        }
+    }
+}
